c8ee375eeee7f52ccf9fa0dd925b0683473029ec
CodingKaiser <falkonoe@live.com>
2017-06-25 10:49:17 -0700

    Added comments to parser.

--- src/L1Parser.java
+++ src/L1Parser.java
@@ -1,14 +1,31 @@
 /**
- * Created by falko on 20-06-17.
+ * A parser that parses the language L1:
+ * (equal # of As and Bs in any order)
+ * Strings supported:
+ * ''
+ * 'ABABAAABBB'
+ * 'AAABBB'
+ * 'ABABAB'
+ *
+ * @author Falko Noe
+ * @version 1.0
  */
 public class L1Parser implements LanguageParser {
 
     private StackLL<Character> st;
 
+    /**
+     * The constructor for the application
+     */
     L1Parser() {
         this.st = new StackLL<>();
     }
 
+    /**
+     * Takes in a character and handles it according to L1
+     * language constraints
+     * @param letter: The character currently being parsed.
+     */
     public void handleLetter(Character letter) {
         if (this.st.isEmpty()) {
             this.st.push(letter);
@@ -27,10 +44,19 @@ public class L1Parser implements LanguageParser {
         }
     }
 
+    /**
+     * Checks whether the string of characters matches the L1 pattern
+     * @return:True if the sequence of characters matches the
+     * L1 pattern, false otherwise
+     */
     public boolean isPatternMatch() {
         return st.isEmpty();
     }
 
+    /**
+     * Resets the stacks of the application in preparation for further
+     * input.
+     */
     public void resetParser() {
         this.st = new StackLL<>();
     }
