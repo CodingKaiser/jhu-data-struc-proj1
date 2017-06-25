4361ac000fb360a0b5ddd86a97f8d5209fced080
CodingKaiser <falkonoe@live.com>
2017-06-25 11:08:38 -0700

    Added comments.

--- src/L5Parser.java
+++ src/L5Parser.java
@@ -1,16 +1,32 @@
-import java.util.Stack;
-
 /**
- * Created by falko on 24-06-17.
+ * A parser that parses the language L5:
+ * xCy
+ * Strings supported:
+ * 'ACA'
+ * 'BCB'
+ * 'C'
+ * 'ABACABA'
+ * 'BABCBAB'
+ *
+ * @author Falko Noe
+ * @version 1.0
  */
 public class L5Parser implements LanguageParser {
 
     StackLL<Character> st;
 
+    /**
+     * The constructor for the application
+     */
     public L5Parser() {
         this.st = new StackLL<>();
     }
 
+    /**
+     * Takes in a character and handles it according to L5
+     * language constraints
+     * @param letter: The character currently being parsed.
+     */
     public void handleLetter(Character letter) {
         if (this.st.isEmpty()) {
             this.st.push(letter);
@@ -29,6 +45,11 @@ public class L5Parser implements LanguageParser {
         }
     }
 
+    /**
+     * Checks whether the string of characters matches the L5 pattern
+     * @return:True if the sequence of characters matches the
+     * L5 pattern, false otherwise
+     */
     public boolean isPatternMatch() {
         Character top = 'B'; // placeholder
         if (this.st.isEmpty()) {
@@ -42,6 +63,10 @@ public class L5Parser implements LanguageParser {
         return false;
     }
 
+    /**
+     * Resets the stacks of the application in preparation for further
+     * input.
+     */
     public void resetParser() {
         this.st = new StackLL<>();
     }
