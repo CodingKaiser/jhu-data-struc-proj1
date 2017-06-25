63c4ef159f705d38409a9f7ab1ed4cfc2cd9d7e9
CodingKaiser <falkonoe@live.com>
2017-06-24 19:36:55 -0700

    Added method documentation and comments.

--- src/StackLL.java
+++ src/StackLL.java
@@ -1,13 +1,29 @@
 /**
- * Created by falko on 20-06-17.
+ * A generic Stack data structure implemented using a LinkedList.
+ * It operates like a usual stack: Users can push to and pop from the
+ * stack in a LIFO behavior.
+ * @author Falko Noe
+ * @version 1.0
  */
 public class StackLL<L> {
 
+    /**
+     * A generic class Node, which defines the
+     * behavior of the LinkedList: each Node carries
+     * a data-value as well as the references to the next item,
+     * which in the case of the stack is an item one step farther
+     * down the Stack.
+     * @param <T>: The datatype the Node is instantiated with
+     */
     class SNode<T> {
 
         T value;
         SNode<T> next;
 
+        /**
+         * The constructor for the Node
+         * @param elem: The data value of datatype T
+         */
         public SNode(T elem) {
             this.value = elem;
             this.next = null;
@@ -18,11 +34,23 @@ public class StackLL<L> {
     private SNode<L> top;
     private int size;
 
+    /**
+     * The Constructor for the Stack, which is instantiated
+     * as empty.
+     */
     public StackLL() {
         this.top = null;
         this.size = 0;
     }
 
+    /**
+     * Pop method which allows the user to remove an item from the top of the
+     * Stack. Returns the data value of the Node that was just popped. Can
+     * throw an exception if the operation is performed on an Empty stack
+     * @return: The data value corresponding to the popped Node.
+     * @throws StackUnderflowException: Thrown if the method is performed on an
+     *                                   empty stack
+     */
     public L pop() throws StackUnderflowException {
         SNode<L> temp = this.top;
         L poppedData;
@@ -37,6 +65,11 @@ public class StackLL<L> {
         }
     }
 
+    /**
+     * Pushes the data value to the top of the stack.
+     * @param dataValue: The datum that is pushed to the top of
+     *                 the Stack.
+     */
     public void push(L dataValue) {
         SNode<L> newTop = new SNode<>(dataValue);
         SNode<L> temp = this.top;
@@ -45,14 +78,28 @@ public class StackLL<L> {
         this.size++;
     }
 
+    /**
+     * Return the current size of the Stack.
+     * @return: The size of the Stack as a function of the
+     * number of items contained within it as an integer.
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Get data value corresponding to the top Node of the Stack
+     * @return: The data value of type L corresponding to the
+     * top of the Stack
+     */
     public L peek() {
         return this.top.value;
     }
 
+    /**
+     * Checks whether the Stack is empty.
+     * @return: True if the stack is empty, false otherwise
+     */
     public boolean isEmpty() {
         return (this.size <= 0);
     }
