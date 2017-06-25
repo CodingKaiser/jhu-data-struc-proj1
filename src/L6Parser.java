f3ef657cc7f5cc5caca6e38e919484ec806f7d85
CodingKaiser <falkonoe@live.com>
2017-06-25 11:21:24 -0700

    Added comments.

--- src/L6Parser.java
+++ src/L6Parser.java
@@ -1,5 +1,14 @@
 /**
- * Created by falko on 24-06-17.
+ * A parser that parses the language L6:
+ * A^nB^nA^n
+ * Strings supported:
+ * ''
+ * 'ABA'
+ * 'AABBAA'
+ * 'AAABBBAAA'
+ *
+ * @author Falko Noe
+ * @version 1.0
  */
 public class L6Parser implements LanguageParser {
 
@@ -7,12 +16,20 @@ public class L6Parser implements LanguageParser {
     StackLL<Character> aOnly;
     StackLL<Character> bOnly;
 
+    /**
+     * The constructor for the application
+     */
     public L6Parser() {
         this.all = new StackLL<>();
         this.aOnly = new StackLL<>();
         this.bOnly = new StackLL<>();
     }
 
+    /**
+     * Takes in a character and handles it according to L6
+     * language constraints
+     * @param letter: The character currently being parsed.
+     */
     public void handleLetter(Character letter) {
         if (this.all.isEmpty() && this.bOnly.isEmpty()) {
             this.all.push(letter);
@@ -35,6 +52,11 @@ public class L6Parser implements LanguageParser {
         }
     }
 
+    /**
+     * Checks whether the string of characters matches the L6 pattern
+     * @return:True if the sequence of characters matches the
+     * L6 pattern, false otherwise
+     */
     public boolean isPatternMatch() {
         try {
             while (!this.all.isEmpty() && !this.aOnly.isEmpty()) {
@@ -49,6 +71,10 @@ public class L6Parser implements LanguageParser {
         return (this.all.isEmpty() && this.aOnly.isEmpty() && this.bOnly.isEmpty());
     }
 
+    /**
+     * Resets the stacks of the application in preparation for further
+     * input.
+     */
     public void resetParser() {
         this.all = new StackLL<>();
         this.aOnly = new StackLL<>();
