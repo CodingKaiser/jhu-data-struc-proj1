79b2c7beb8b2c64094e3d753c2d2db54ff75e7ed
CodingKaiser <falkonoe@live.com>
2017-06-25 21:54:44 -0700

    Changed 4 spaces to 2 spaces.

--- src/StackUnderflowException.java
+++ src/StackUnderflowException.java
@@ -8,13 +8,13 @@
  */
 class StackUnderflowException extends Exception {
 
-    /**
-     * Simply calls Exception constructor for
-     * displaying the provided message to stderr
-     * @param message: The string explaining the error
-     *               and why it occurred.
-     */
-    StackUnderflowException(String message) {
-        super(message);
-    }
+  /**
+   * Simply calls Exception constructor for
+   * displaying the provided message to stderr
+   * @param message: The string explaining the error
+   *                 and why it occurred.
+   */
+  StackUnderflowException(String message) {
+    super(message);
+  }
 }
